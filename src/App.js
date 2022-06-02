64dfd8012266b8f1dc7476ba96d02f85ec5e142a
sujal-subedi <sujal.subedi0605@gmail.com>
2022-06-02 14:02:21 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [Count, setCount] = useState("");
+
+  let noc = Count === "" ? "" : Count.length;
+  let now = Count.match(/(\w+)/g) || [];
+  now = Count === "" ? "" : now.length;
+
+  let nos = Count === "" ? "" : Count.split(" ").length - 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="title">
+        <h2>Word Count App</h2>
+      </div>
+      <textarea
+        placeholder="Write Here..."
+        onChange={(e) => setCount(e.target.value)}
+      ></textarea>
+      <p>Total number of Character is {noc}</p>
+      <p>Total number of Words is {now}</p>
+      <p>Total number of Space is {nos}</p>
+    </>
   );
 }
 
